2271e635d254a7e5b2871573075e18667cd068b3
“saschakala” <mxsaschakala@gmail.com>
2020-08-30 09:53:30 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,44 +1,42 @@
-
 class CashRegister
+attr_accessor :total, :discount, :title, :price, :quantity, :items, :total_before_add
 
-    attr_accessor :total, :discount, :last_transaction_amount, :items
-  
-    def initialize(discount = 0)
-      @total = 0
-      @discount = discount
-      @items = []
-    end
-  
-  
-    def add_item(title,price,quantity = 1)
-      if quantity>1
-        i=0
-        while i<quantity
-          @items << title
-          i+=1
-        end
-      else
-        @items << title
-      end
-      @last_transaction_amount = @total
-      @total += price*quantity
-      @total
-    end
-  
-    def apply_discount()
-      if @discount > 0
-        @discount = @discount/100.to_f
-        @total = @total - (@total * (@discount))
-        "After the discount, the total comes to $#{@total.to_i}."
-      else
-        "There is no discount to apply."
-      end
-    end
-  
-  
-    def void_last_transaction()
-      @total = @last_transaction_amount
-    end
-  
+
+def initialize (discount=0)
+  @total = 0
+  @discount = discount
+  @items = []
+end
+
+def add_item (title, price, quantity = 1)
+  @total_before_add = @total #sets a variable to keep track of the total amount
+  #before the next item is added
+  @total += price * quantity
+  quantity.times {@items << title} 
+  #iterates over the block a quantity amount of times so that the correct
+  #count of each individual item is added
+end
+
+def apply_discount
+  if @discount > 0
+  @total -= (@total * @discount.to_f / 100).to_i
+  "After the discount, the total comes to $#{total}."
+  else 
+    "There is no discount to apply."
   end
-  
+end
+
+def items
+  @items
+end
+
+def void_last_transaction
+  @total = @total_before_add #sets the total equal to the total before the 
+  #last item was added
+end
+
+
+
+
+
+end
